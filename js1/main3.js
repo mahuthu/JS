3c68df5824d49f710fbcf66bdd541c47d7091a20
mahuthu <mahuthup@gmail.com>
2024-01-11 10:21:37 +0300

    version3

--- js1/main3.js
+++ js1/main3.js
@@ -43,3 +43,40 @@ function playGame(){
 }
 
 playGame();
+
+
+
+let playGame = confirm("do you want to play a game?")
+if(playGame){
+    let playerChoice = prompt("please enter rock,  paper, scissors")
+    if (playerChoice){
+        let playerOne = playerChoice.trim().toLowerCase();
+        if (playerOne === "rock" || playerOne === "paper" || playerOne === "scissors"){
+            let computerChoice = Math.floor(Math.random()*3 + 1);
+            let computer = computerChoice === 1 ? "rock" : computerChoice === 2 ? "paper" : "scissors" ;
+            let results =
+            playerOne === computer
+                ? "tie"
+                : playerOne === "rock" && computer === "paper"
+                ? `playerOne: ${playerOne}\ncomputer: ${computer}\ncomputer wins`
+                : playerOne === "paper" && computer === "scissors"
+                ? `playerOne: ${playerOne}\ncomputer: ${computer}\ncomputer wins`
+                : playerOne === "scissors" && computer === "rock"
+                ? `playerOne: ${playerOne}\ncomputer: ${computer}\ncomputer wins`
+                : "player1 wins";
+
+            alert(results)
+            
+            let  playAgain = confirm("do you want to play again?")
+            playAgain ? location.reload() : alert("maybe next time")
+        }
+
+    }
+    else{
+        alert("invalid input")
+    }
+}
+
+else{
+    alert("maybe next time")
+}
